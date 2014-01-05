9460d726cd2bd86d8aef1656fdbe1ba1771c4489
hak8or <hak8or@gmail.com>
2014-01-05 12:42:12 -0500

    Fixed script spacing and log directory

--- xolo_miner_setup.sh
+++ xolo_miner_setup.sh
@@ -27,8 +27,10 @@ XRam_Pool_IP="xpool.xram.co"
 XRam_Pool_Port="1339"
 
 # Start logging.
-touch $HOME/xolo_miner_setup.log
+touch xolo_miner_setup.log
 
+# Handles the input from command line when running the script. Decides if testing
+# is enabled, what pool to use, and what primecoin address to mine too.
 while getopts ":a:tp:" opt; do
 	case $opt in
 	    a)
@@ -62,108 +64,129 @@ while getopts ":a:tp:" opt; do
         echo "Using XRam at $Pool_IP port $Pool_Port" &>>xolo_miner_setup.log
       else
         echo "|         Pool unknown. Please specify Beeeeer or XRam.             |"
+        echo "+-------------------------------------------------------------------+"
 	      exit 1
       fi
       ;;
 	    \?)
 		  echo "|    Invalid option: -$OPTARG                                      |"
+		  echo "+-------------------------------------------------------------------+"
 	      exit 1
 	      ;;
 	    :)
 	      echo "|    You need to put in an address!                                |"
+	      echo "+-------------------------------------------------------------------+"
 	      exit 1
 	      ;;
 	esac
 done
 
-# This checks if an address was given via command line argument. If not, ask user.
-if [ -z "$PrimeCoin_Address" ]; then
-	echo -n "|  PrimeCoin address?    :"
-	read PrimeCoin_Address
-	optarg_length=$(echo ${#PrimeCoin_Address})
-	if [ $optarg_length -ne 34 ]; then
-		echo "|           Address is not 34 characters, exiting.                  |"
-		echo "+-------------------------------------------------------------------+"
-		exit 1
+# Checks if an address was given via command line argument. If not, ask user.
+	if [ -z "$PrimeCoin_Address" ]; then
+		echo -n "|  PrimeCoin address?    :"
+		read PrimeCoin_Address
+		optarg_length=$(echo ${#PrimeCoin_Address})
+		if [ $optarg_length -ne 34 ]; then
+			echo "|           Address is not 34 characters, exiting.                  |"
+			echo "+-------------------------------------------------------------------+"
+			exit 1
+		fi
 	fi
-fi
-#This checks to see if a pool was given via command line argument. If not, set to a default.
-if [ -z "$Pool_IP" ]; then
-	#Set a default pool address
-	Pool_IP=$Beeeeer_US_Pool_IP
-	Pool_Port=$Beeeeer_US_Pool_Port
-	echo "|                    Using Beeeeer.org US pool.                     |"
-	echo "Using default pool, Beeeeer.org at $Pool_IP port $Pool_Port" &>>xolo_miner_setup.log
-fi
-read -p "|                   Press [ENTER] to continue?                      |"
-echo "+-------------------------------------------------------------------+"
-echo ""
 
-echo "  [0/6] Installing required packages."
-echo "----FROM SCRIPT ECHO---- Installing required packages." &>>xolo_miner_setup.log
-apt-get update &>/dev/null
-apt-get install yasm -y git make g++ build-essential libminiupnpc-dev libboost-all-dev libdb++-dev libgmp-dev libssl-dev dos2unix htop supervisor &>>xolo_miner_setup.log
-# It seems that primeminer does not handle lboost_chrono and similar from newer libboost on lower distributions, so libboost 1.48 has to be installed for them. 
-# More info can be found here: http://www.peercointalk.org/index.php?topic=501.165
-# Do a check in the future here to see if this needs to be done.
-apt-get install -y libboost-chrono1.48-dev libboost-filesystem1.48-dev libboost-system1.48-dev libboost-program-options1.48-dev libboost-thread1.48-dev &>>xolo_miner_setup.log
+# Checks to see if a pool was given via command line argument. If not, set to a default.
+	if [ -z "$Pool_IP" ]; then
+		#Set a default pool address
+		Pool_IP=$Beeeeer_US_Pool_IP
+		Pool_Port=$Beeeeer_US_Pool_Port
+		echo "|                   Using Beeeeer.org US pool.                      |"
+		echo "----FROM SCRIPT ECHO---- Using default pool, Beeeeer.org at $Pool_IP port $Pool_Port" &>>xolo_miner_setup.log
+	fi
 
-echo "  [1/6] Downloading miner source from git"
-echo "----FROM SCRIPT ECHO---- Downloading miner source from git" &>>xolo_miner_setup.log
-git clone https://github.com/thbaumbach/primecoin.git &>>xolo_miner_setup.log
+# Ask the user if the address is good and if to continue.
+	read -p "|                   Press [ENTER] to continue?                      |"
+	echo "+-------------------------------------------------------------------+"
+
+
+# Installs all the requires packages to compile and run primecoin miner.
+	echo "  [0/6] Installing required packages."
+	echo "----FROM SCRIPT ECHO---- Installing required packages." &>>xolo_miner_setup.log
+	apt-get update &>/dev/null
+	apt-get install yasm -y git make g++ build-essential libminiupnpc-dev libboost-all-dev libdb++-dev libgmp-dev libssl-dev dos2unix htop supervisor &>>xolo_miner_setup.log
+	
+	# It seems that primeminer does not handle lboost_chrono and similar from newer libboost on lower distributions, so libboost 1.48 has to be installed for them. 
+	# More info can be found here: http://www.peercointalk.org/index.php?topic=501.165
+	# Do a check in the future here to see if this needs to be done.
+	apt-get install -y libboost-chrono1.48-dev libboost-filesystem1.48-dev libboost-system1.48-dev libboost-program-options1.48-dev libboost-thread1.48-dev &>>xolo_miner_setup.log
+
+# Clones the github repo for primecoin miner.
+	echo "  [1/6] Downloading miner source from git"
+	echo "----FROM SCRIPT ECHO---- Downloading miner source from git" &>>xolo_miner_setup.log
+	git clone https://github.com/thbaumbach/primecoin.git &>>xolo_miner_setup.log
 
 # Increase the swapfile size to 1 GB so primecoin will be able to compile if encountering less than 512 MB of ram.
-memory_size_KB=$(grep MemTotal /proc/meminfo | awk '{print $2}'  )
-if [ $memory_size_KB -lt 1048576 ]; then
-    echo "  [2/6] Changing swapfile size so miner can compile with less than 512MB of ram"
-    echo "----FROM SCRIPT ECHO---- Changing swapfile size so miner can compile with less than 512MB of ram" &>>xolo_miner_setup.log
-	dd if=/dev/zero of=/swapfile bs=64M count=16 &>>xolo_miner_setup.log
-	mkswap /swapfile &>>xolo_miner_setup.log
-	swapon /swapfile &>>xolo_miner_setup.log
-else
-    echo "[2/6] Not increasing swapfile size."
-    echo "----FROM SCRIPT ECHO---- Changing swapfile size so miner can compile with less than 512MB of ram" &>>xolo_miner_setup.log
-fi
+	memory_size_KB=$(grep MemTotal /proc/meminfo | awk '{print $2}'  )
+	if [ $memory_size_KB -lt 1048576 ]; then
+	    echo "  [2/6] Changing swapfile size so miner can compile with less than 512MB of ram"
+	    echo "----FROM SCRIPT ECHO---- Changing swapfile size so miner can compile with less than 512MB of ram" &>>xolo_miner_setup.log
+		dd if=/dev/zero of=/swapfile bs=64M count=16 &>>xolo_miner_setup.log
+		mkswap /swapfile &>>xolo_miner_setup.log
+		swapon /swapfile &>>xolo_miner_setup.log
+	else
+	    echo "  [2/6] Not increasing swapfile size"
+	    echo "----FROM SCRIPT ECHO---- Not increasing swapfile size" &>>xolo_miner_setup.log
+	fi
 
-echo "  [3/6] Compiling miner on $Processor_Count core(s), this may take a while."
-echo "----FROM SCRIPT ECHO---- Compiling miner on $Processor_Count core(s), this may take a while." &>>xolo_miner_setup.log
-cd ~/primecoin/src &>/dev/null
-#Change the compilier optimization flag from 2 to 3 (Small increase in PPS/Chains per day).
-sed -i 's/-O2/-O3/g' makefile.unix&>/dev/null 
-make -j $Processor_Count -f makefile.unix &>/dev/null
+# Compile the miner and split the compilation process into jobs so each core gets its own job. Makes compilation quicker.
+	echo "  [3/6] Compiling miner on $Processor_Count core(s), this may take a while."
+	echo "----FROM SCRIPT ECHO---- Compiling miner on $Processor_Count core(s), this may take a while." &>>xolo_miner_setup.log
+	cd primecoin/src &>/dev/null
 
-echo "  [4/6] Setting up autostart for miner if server restarts or whatever"
-echo "----FROM SCRIPT ECHO---- Setting up autostart for miner if server restarts or whatever" &>>xolo_miner_setup.log
-mkdir -p /var/log/supervisor &>>xolo_miner_setup.log
-touch /etc/supervisor/conf.d/primecoin.conf &>>xolo_miner_setup.log
-cat <<- _EOF_ >/etc/supervisor/conf.d/primecoin.conf
-[program:primecoin]
-command=$HOME/primecoin/src/primeminer -pooluser=$PrimeCoin_Address -poolip=$Pool_IP -poolport=$Pool_Port -genproclimit=$Processor_Count -poolpassword=PASSWORD -poolshare=6
-stdout_logfile=/var/log/supervisor/%(program_name)s.log
-stderr_logfile=/var/log/supervisor/%(program_name)s.log
-autorestart=true
+	# Change the compilier optimization flag from 2 to 3 (Small increase in PPS/Chains per day).
+	sed -i 's/-O2/-O3/g' makefile.unix&>/dev/null 
+	make -j $Processor_Count -f makefile.unix &>/dev/null
+
+	# Go back to the parent directory since we don't need to be in primecoin/src/ anymore.
+	cd ../.. &>/dev/null
+
+# Tells the supervisor to restart primecoin miner if the server restarts.
+	echo "  [4/6] Setting up autostart for miner if server is restarted"
+	echo "----FROM SCRIPT ECHO---- Setting up autostart for miner if server is restarted" &>>xolo_miner_setup.log
+	mkdir -p /var/log/supervisor &>>xolo_miner_setup.log
+	touch /etc/supervisor/conf.d/primecoin.conf &>>xolo_miner_setup.log
+
+	cat <<- _EOF_ >/etc/supervisor/conf.d/primecoin.conf
+		[program:primecoin]
+		command=$HOME/primecoin/src/primeminer -pooluser=$PrimeCoin_Address -poolip=$Pool_IP -poolport=$Pool_Port -genproclimit=$Processor_Count -poolpassword=PASSWORD -poolshare=6
+		stdout_logfile=/var/log/supervisor/%(program_name)s.log
+		stderr_logfile=/var/log/supervisor/%(program_name)s.log
+		autorestart=true
 _EOF_
 
-echo "  [5/6] Restarting supervisor"
-echo "----FROM SCRIPT ECHO---- Restarting supervisor" &>>xolo_miner_setup.log
-/etc/init.d/supervisor stop &>>xolo_miner_setup.log
-/etc/init.d/supervisor start &>>xolo_miner_setup.log
+# Restarts the supervisor so the previous changes will take effect.
+	echo "  [5/6] Restarting supervisor"
+	echo "----FROM SCRIPT ECHO---- Restarting supervisor" &>>xolo_miner_setup.log
+	/etc/init.d/supervisor stop &>>xolo_miner_setup.log
+	/etc/init.d/supervisor start &>>xolo_miner_setup.log
 
-echo "  [6/6] Creating a script to force primeminer to restart in case it freezes"
-echo "----FROM SCRIPT ECHO---- Creating a script to force primeminer to restart in case it freezes" &>>xolo_miner_setup.log
-touch ~/mine_watcher.sh
-cat <<- _EOF_ >~/mine_watcher.sh
-while true; do
-  if [[ \`tail -n 1 /var/log/supervisor/primecoin.log | cut -c1-27\` == "force reconnect if possible" ]]; then
-    killall primeminer
-  fi
-  sleep 15
-done
+# Checks to see if primecoin miner wants to restart itself due to connectivity issues.
+	echo "  [6/6] Creating a script to force primeminer to restart in case it freezes"
+	echo "----FROM SCRIPT ECHO---- Creating a script to force primeminer to restart in case it freezes" &>>xolo_miner_setup.log
+	touch ~/mine_watcher.sh
+
+	cat <<- _EOF_ >~/mine_watcher.sh
+	while true; do
+	  if [[ \`tail -n 1 /var/log/supervisor/primecoin.log | cut -c1-27\` == "force reconnect if possible" ]]; then
+	    killall primeminer
+	  fi
+	  sleep 15
+	done
 _EOF_
-chmod 755 ~/mine_watcher.sh
-~/mine_watcher.sh &
 
-echo "----FROM SCRIPT ECHO----     SCRIPT IS COMPLETE    " &>>xolo_miner_setup.log
+	chmod 755 ~/mine_watcher.sh
+	~/mine_watcher.sh &
+
+# The script is now done!
+	echo "----FROM SCRIPT ECHO----     SCRIPT IS COMPLETE    " &>>xolo_miner_setup.log
 
 echo "+-------------------------------------------------------------------------------+"
 echo "|         All done!                                                             |"
